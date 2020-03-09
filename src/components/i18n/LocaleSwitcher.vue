72c6d5f20bf40b7e3a33c5026cbecb5fc676d4a6
Miroslav Bauer <mirekys@gmail.com>
2020-03-09 12:56:48 +0100

    add site meta tags, dynamic title & locale support

---
+++ src/components/i18n/LocaleSwitcher.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <q-select
+    v-model="lang"
+    :options="langOptions"
+    label="Quasar Language"
+    dense
+    borderless
+    emit-value
+    map-options
+    options-dense
+    style="min-width: 150px"
+  />
+  <!-- ...... -->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      lang: this.$i18n.locale,
+      langOptions: [
+        { value: 'en-us', label: 'English' },
+        { value: 'cs-cz', label: 'Czech' }
+      ]
+    }
+  },
+  watch: {
+    lang (lang) {
+      this.$i18n.locale = lang
+    }
+  }
+}
+</script>
